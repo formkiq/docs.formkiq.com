2263230193f22315dbf680fed5b734e68013ec21
Mike Friesen <mfriesen@gmail.com>
2023-08-18 14:39:11 -0500

    added gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,6 +37,10 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        gtag: {
+          trackingID: 'G-TSEZN0FVNG',
+          anonymizeIP: true,
+        },
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
