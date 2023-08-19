51d971edd95de4480d24b710e5e6c153734e0860
Mike Friesen <mfriesen@gmail.com>
2023-08-18 22:17:25 -0500

    update edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/formkiq/docs.formkiq.com/tree/main/docs/',
+            'https://github.com/formkiq/docs.formkiq.com/tree/main/',
           docLayoutComponent: "@theme/DocPage",
           docItemComponent: "@theme/ApiItem" // Derived from docusaurus-theme-openapi
         },
