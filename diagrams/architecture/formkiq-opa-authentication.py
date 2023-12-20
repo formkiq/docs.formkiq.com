4cf127e29ec1bb48f4b79e6dc030c7842d5f1f4d
Mike Friesen <mfriesen@gmail.com>
2023-12-19 22:35:10 -0600

    Added Open Policy documentation

---
+++ diagrams/architecture/formkiq-opa-authentication.py
@@ -0,0 +1,32 @@
+from diagrams import Cluster, Diagram, Edge, Node
+from diagrams.onprem.client import Client
+from diagrams.generic.device import Mobile
+from diagrams.onprem.client import Users
+from diagrams.aws.network import APIGateway
+from diagrams.aws.database import Dynamodb
+from diagrams.aws.storage import SimpleStorageServiceS3BucketWithObjects
+from diagrams.aws.compute import Lambda
+from diagrams.aws.compute import Fargate
+from diagrams.aws.analytics import ES
+from diagrams.custom import Custom
+from diagrams.aws.integration import SNS, SQS 
+from diagrams.aws.ml import Textract
+
+graph_attr = {
+    "layout":"dot",
+    "compound":"true",
+}
+
+with Diagram("FormKiQ OPA Authentication", show=False, graph_attr=graph_attr, direction="TB"):
+
+    opa = Custom('Open Policy Agent', './opa.png')
+
+    with Cluster("API Gateway"):
+        api1 = APIGateway("JWT Authorization") 
+        api2 = APIGateway("IAM Authorization") 
+        api3 = APIGateway("KEY Authorization")
+
+    Edge(ltail='cluster_Applications',lhead='cluster_API Gateway') >> api1 >> opa
+    Edge(ltail='cluster_Applications',lhead='cluster_API Gateway') >> api2 >> opa
+    Edge(ltail='cluster_Applications',lhead='cluster_API Gateway') >> api3 >> opa
+
