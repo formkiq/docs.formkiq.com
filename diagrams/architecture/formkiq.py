9329244b0bdf6ce81de7ddaddff44419ef52ba15
Mike Friesen <mfriesen@gmail.com>
2025-03-11 17:06:11 -0500

    Updated FormKiQ Architecture diagram

--- diagrams/architecture/formkiq.py
+++ diagrams/architecture/formkiq.py
@@ -15,15 +15,15 @@ from diagrams.aws.compute import Lambda
 from diagrams.aws.compute import Fargate
 from diagrams.aws.analytics import ES
 from diagrams.custom import Custom
-from diagrams.aws.integration import SNS, SQS 
-from diagrams.aws.ml import Textract
+from diagrams.aws.integration import SNS, SQS, Eventbridge
+from diagrams.aws.ml import Textract, Bedrock, Comprehend
 
 graph_attr = {
     "layout":"dot",
     "compound":"true",
 }
 
-with Diagram("FormKiQ", show=False, graph_attr=graph_attr):
+with Diagram("FormKiQ Architecture", show=False, graph_attr=graph_attr):
 
     with Cluster("API Gateway", direction="TB"):
         api1 = APIGateway("JWT Authorization") 
@@ -35,48 +35,57 @@ with Diagram("FormKiQ", show=False, graph_attr=graph_attr):
         mobile = Mobile("Mobile")
         users = Users("Users")
 
-    with Cluster("Additional Actions and Modules"):
+    with Cluster("Additional FormKiQ Features and Modules", graph_attr={"margin": "20"}):
         with Cluster("Fulltext Search"):
-            opensearch = ES("OpenSearch")
+            opensearch = ES("OpenSearch Managed\n/ Serverless")
             typesense = Custom('Typesense', './typesense.png')
             opensearch - Edge(label="OR", style="dotted") - typesense
 
-        with Cluster("Optical Character Recognition"):
+        with Cluster("Optical Character Recognition", graph_attr={"margin": "20"}):
             tesseract = Custom("Tesseract OCR","./tesseract.png")
             textract = Textract("AWS Textract")
             textract - Edge(label="OR", style="dotted") - tesseract
 
-        with Cluster("Intelligent Document Classification / Anti-Malware / E-Signature"):
-            openai = Custom("OpenAI","./openai.png")
+        with Cluster("Anti-Malware", graph_attr={"margin": "20"}):
             antimalware = Custom("Anti-Malware","./anti-malware.png")
+
+        with Cluster("E-Signature", graph_attr={"margin": "20"}):
             docusign = Custom("Docusign","./docusign.png")
-            openai - Edge(style="dashed") - antimalware - Edge(style="dashed") - docusign
 
-    with Cluster("FormKiQ"):
+        with Cluster("Intelligent Document Processing", graph_attr={"margin": "20"}):
+            comprehend = Comprehend("Amazon Comprehend")
+            bedrock = Bedrock("Amazon Bedrock")
+            comprehend - Edge(style="dashed") - bedrock
 
-        with Cluster("Document Storage"):
+        antimalware - Edge(ltail='cluster_Anti-Malware',lhead='cluster_E-Signature') - docusign
+
+    with Cluster("FormKiQ Core"):
+
+        with Cluster("Document Storage", graph_attr={"margin": "20"}):
             storage = Dynamodb("Amazon DynamoDB") 
             s3 = SimpleStorageServiceS3BucketWithObjects("Amazon S3")
-            storage >> s3
+            storage - Edge(style="dashed") - s3
 
-        with Cluster("Serverless Processing"):
-            processing = Lambda("AWS Lambda") 
-            processing2 = Lambda("AWS Lambda")
-            processing - Edge(style="dashed") - processing2
+        with Cluster("Serverless Processing", graph_attr={"margin": "20"}):
+            processing = Lambda("RESTful API") 
+            actions = Lambda("Document Actions")
+            processing >> actions
 
-        with Cluster("Document Events"):
+        with Cluster("Document Events", graph_attr={"margin": "20"}):
+            messagingEventBridge = Eventbridge("Amazon Eventbridge")
             messagingSns = SNS("Amazon SNS")
             messagingSqs = SQS("Amazon SQS")
-            messagingSns - Edge(style="dashed") - messagingSqs
+            messagingEventBridge >> messagingSns >> messagingSqs
+
 
     web >> Edge(ltail='cluster_Applications',lhead='cluster_API Gateway') >> api1
     mobile >> Edge(ltail='cluster_Applications',lhead='cluster_API Gateway') >> api2
     users >> Edge(ltail='cluster_Applications',lhead='cluster_API Gateway') >> api3
 
-    api1 >> Edge(ltail='cluster_API Gateway',lhead='cluster_FormKiQ') >> storage
-    api2 >> Edge(ltail='cluster_API Gateway',lhead='cluster_FormKiQ') >> processing
-    api3 >> Edge(ltail='cluster_API Gateway',lhead='cluster_FormKiQ') >> messagingSns
+    api1 >> Edge(ltail='cluster_API Gateway',lhead='cluster_FormKiQ Core') >> processing
+    api2 >> Edge(ltail='cluster_API Gateway',lhead='cluster_FormKiQ Core') >> messagingEventBridge
+    api3 >> Edge(ltail='cluster_API Gateway',lhead='cluster_FormKiQ Core') >> storage
 
-    s3 >> Edge(ltail='cluster_FormKiQ',lhead='cluster_Fulltext Search') >> opensearch
-    processing2 >> Edge(ltail='cluster_FormKiQ',lhead='cluster_Optical Character Recognition') >> textract
-    messagingSqs >> Edge(ltail='cluster_FormKiQ',lhead='cluster_Additional Actions and Modules') >> openai
+    messagingSqs >> Edge(ltail='cluster_FormKiQ Core',lhead='cluster_Additional FormKiQ Features and Modules') >> antimalware
+    actions >> Edge(ltail='cluster_FormKiQ Core',lhead='cluster_Additional FormKiQ Features and Modules') >> textract
+    s3 >> Edge(ltail='cluster_FormKiQ Core',lhead='cluster_Additional FormKiQ Features and Modules') >> opensearch
